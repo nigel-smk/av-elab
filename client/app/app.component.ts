b40b9cdf791001d34b481c084c5908c67e3b9874
Nigel Maynard <kahler.maynard@gmail.com>
2017-10-02 21:38:14 -0400

    removed example classes from starter

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +7,6 @@ import { AuthService } from './services/auth.service';
 })
 export class AppComponent {
 
-  constructor(public auth: AuthService) { }
+  constructor() { }
 
 }

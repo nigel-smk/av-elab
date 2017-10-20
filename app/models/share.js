bc2902acb09d980be2c4a5d58e86fbb191d2028c
Nigel Maynard <kahler.maynard@gmail.com>
2017-10-19 21:25:46 -0400

    clearing master

--- app/models/share.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var shareSchema = mongoose.Schema({
-    name: String,
-    email: String,
-    permission: String
-});
-
-module.exports = mongoose.model('Share', shareSchema);

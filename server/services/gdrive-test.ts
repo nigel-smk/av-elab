1c8aeb580e80c3bc027dd355e998dca3a2e7f1f6
Nigel Maynard <kahler.maynard@gmail.com>
2017-11-27 21:44:02 -0500

    adds gdrive-test changes

--- server/services/gdrive-test.ts
+++ server/services/gdrive-test.ts
@@ -8,12 +8,12 @@ async function run() {
   // await gdrive.listFiles();
   await gdrive.init();
   // await gdrive.mkDir(['promiseTest']);
-  // await gdrive.unLink(['promiseTest']);
+  await gdrive.unLink(['promiseTest']);
   // await gdrive.mkDir(['promiseTest']);
-  for (let x=0;x<80;x++) {
-    console.log(x);
-    gdrive.writeFile(['promiseTest', 'cat3.jpg'], fs.createReadStream('/home/nigel/dev/projects/av-elab/server/services/cat.jpg'));
-  }
+  // for (let x=0;x<80;x++) {
+  //   console.log(x);
+  //   gdrive.writeFile(['promiseTest', 'cat3.jpg'], fs.createReadStream('/home/nigel/dev/projects/av-elab/server/services/cat.jpg'));
+  // }
   // console.log(await gdrive.writeFile(['promiseTest', 'cat3.jpg'], fs.createReadStream('/home/nigel/dev/projects/av-elab/server/services/cat.jpg')));
   // await gdrive.createPermission(['promiseTest'], 'kahler.maynard@gmail.com', 'reader', 'user');
   // await gdrive.unLinkById('0B89mm4BwiIloejRnMGRHZkRxbDQ');

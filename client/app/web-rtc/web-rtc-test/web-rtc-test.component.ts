d72e0ce55ccb47e7ee4ded8891cdc8634d77b536
Nigel Maynard <kahler.maynard@gmail.com>
2017-12-20 22:06:31 -0500

    messy localstorage implementation

--- client/app/web-rtc/web-rtc-test/web-rtc-test.component.ts
+++ client/app/web-rtc/web-rtc-test/web-rtc-test.component.ts
@@ -3,7 +3,7 @@ import {UserMediaService} from '../services/user-media.service';
 import {PcmDataService} from '../services/pcm-data.service';
 import {Mp3BlobService} from '../services/mp3-blob.service';
 import {Observable} from 'rxjs/Observable';
-import {DomSanitizer} from '@angular/platform-browser';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-web-rtc-test',
@@ -16,7 +16,10 @@ export class WebRtcTestComponent implements OnInit, AfterViewChecked {
 
   @ViewChild('playback') playback: ElementRef;
   @ViewChild('playbackSource') playbackSource: ElementRef;
+  @ViewChild('playbackSourceStored') playbackSourceStored: ElementRef;
   private prevAudioSrc: String;
+
+  public storedMp3: SafeResourceUrl;
 
 
   constructor(private userMedia: UserMediaService,
@@ -25,10 +28,28 @@ export class WebRtcTestComponent implements OnInit, AfterViewChecked {
               private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
+
+    const stored = localStorage.getItem("mp3");
+    if (stored) {
+      this.storedMp3 = this.sanitizer.bypassSecurityTrustResourceUrl(stored);
+      this.playback.nativeElement.load();
+    }
+
+
     this.userMedia.getUserMedia();
 
     this.mp3Url$ = this.mp3Blob.$
       .map((blob: Blob) => {
+      // TODO put in a function
+        const fr = new FileReader();
+        fr.onload = (e) => {
+          if (e.type === 'load') {
+            console.log(fr.result);
+            localStorage.setItem("mp3", fr.result);
+          }
+        };
+        fr.readAsDataURL(blob);
+
         return this.sanitizer.bypassSecurityTrustResourceUrl(URL.createObjectURL(blob));
       })
       .do((blob) => console.log(blob), () => console.log("error"), () => console.log("complete"));
